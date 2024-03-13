d693bef62b3e8544036ecbe3986a3f3a12ee46a8
Willfredy Vieira Dias <willfredvd@gmail.com>
2024-03-14 00:27:47 +0100

    Modo Escuro a 90% e Sobre Nós completo com sucesso!

---
+++ js/darkpage.js
@@ -0,0 +1,5 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    // Verifica a cor atual de fundo e alterna conforme necessário
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'rgb(17, 24, 39)' ? '#ffffff' : '#111827';
+  });
+  
