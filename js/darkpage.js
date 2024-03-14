ac0bae158f5413be2b4ee7662bda7a9481e34fd6
Willfredy Vieira Dias <willfredvd@gmail.com>
2024-03-14 02:46:10 +0100

    Modo Escuro Completo e Página de Registo Completa

--- js/darkpage.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    // Verifica a cor atual de fundo e alterna conforme necessário
-    document.body.style.backgroundColor = document.body.style.backgroundColor === 'rgb(17, 24, 39)' ? '#ffffff' : '#111827';
-  });
-  
